7b3ff3a7495f22c90cd30756d37ba7819319f269
Sswordisasoongamedev <154607248+Sswordisasoongamedev@users.noreply.github.com>
2024-04-06 21:21:35 +0800

    Update cookie.js

--- test_folder/cookie.js
+++ test_folder/cookie.js
@@ -1,4 +1,4 @@
-import { add_cookie, getCookieValue, substringsplit, charsplit } from ".cookies.js";
+import { add_cookie, getCookieValue, substringsplit, charsplit } from "cookies.js";
 
       
         // Now you can use the imported functions
