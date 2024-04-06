12b0cc4b39c0d140a86268c76d668b922af817a0
Sswordisasoongamedev <154607248+Sswordisasoongamedev@users.noreply.github.com>
2024-04-06 21:51:16 +0800

    trying to fix this thing]

--- test_folder/cookie.js
+++ test_folder/cookie.js
@@ -1,12 +1,16 @@
-import { add_cookie, getCookieValue, substringsplit, charsplit } from "cookies.js";
+import { add_cookie, getCookieValue, substringsplit, charsplit } from "./cookies.js";
 
       
-        
+        // Now you can use the imported functions
         const username = getCookieValue("Username");
         console.log(username);
         function test(){
             add_cookie("Username = Ssword(dev)")
+
+        }
+        function aftest(){
+          returned =getCookieValue("Username")
+          return returned
         }
 
-export default test
-
+export { test,aftest }
