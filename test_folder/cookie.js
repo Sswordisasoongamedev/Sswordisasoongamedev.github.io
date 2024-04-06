8c4e288de487b326248900f52c303bf8659498fa
Sswordisasoongamedev <154607248+Sswordisasoongamedev@users.noreply.github.com>
2024-04-06 21:38:05 +0800

    Update cookie.js

--- test_folder/cookie.js
+++ test_folder/cookie.js
@@ -1,14 +1,12 @@
 import { add_cookie, getCookieValue, substringsplit, charsplit } from "cookies.js";
 
       
-        // Now you can use the imported functions
+        
         const username = getCookieValue("Username");
         console.log(username);
         function test(){
             add_cookie("Username = Ssword(dev)")
         }
-<<<<<<< HEAD
-export default test
-=======
 
->>>>>>> 7b3ff3a7495f22c90cd30756d37ba7819319f269
+export default test
+
