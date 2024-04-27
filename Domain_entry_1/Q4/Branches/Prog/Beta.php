0c5643c861fb3c8371d2860232f562319b010eee
Sswordisasoongamedev <154607248+Sswordisasoongamedev@users.noreply.github.com>
2024-04-27 18:49:20 +0800

    added php

---
+++ Domain_entry_1/Q4/Branches/Prog/Beta.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>A li'l trick i have in my sleeves</title>
+    <!--<script type="module">
+        import { aftest } from "../../../../test_folder/cookie.js";
+        /**
+         * @returns {string|null}
+        */
+        const cookie = window.funx = function() {
+            aftest()
+        }
+    </script>-->
+    <link rel="stylesheet" href="Sublevels/sublevels.css">
+    <script src="../../../../javascript/cookies.js">
+        
+    </script>
+    <script>
+        
+    </script>
+</head>
+<body class="alpha-gen dark" >
+    <div class="greetings">
+        <div class="text-greetings" id="af">
+
+        <?php echo("Hello there :" . $_GET["name"] . "it is nice to see you on the other side") ?>
+        </div>
+    </div>
+    
+</body>
+</html>
